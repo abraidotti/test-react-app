887b667c4cc2ce4d47434cba884e1e17fc87307f
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-01-29 13:15:38 -0500

    add a knight...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Knight from './Knight'
+//import './index.css';
+//import App from './App';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Knight />, document.getElementById('root'));
+//registerServiceWorker();
