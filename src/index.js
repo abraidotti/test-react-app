baff4f1280de06ec4b7b6024e540db5f0060ade5
Nicole W <nicolemairead4@gmail.com>
2018-01-29 13:13:57 -0500

    add chess board

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
+//import React from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Board from './Board';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+export default class Board extends Component {
+  render (){
+    return (
+      <div>
+        <Square black>
+          <Knight />
+        </Square>
+      </div>
+    );
+  }
+}
+
+Board.propTypes = {
+  knightPosition: PropTypes.arrayOf(
+    PropTypes.number.isRequired
+  ).isRequired
+};
+
+ReactDOM.render(
+  <Square black>
+    <Knight />
+  </Square>,
+  document.getElementById('root'));
+
+
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
